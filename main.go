ed1d6b0203adb69741a222fc54bdfbf1b477ac58
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-09-29 12:17:22 -0500

    Funciona obtener e

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main(){
+
+	var n int
+	fmt.Scan(&n)
+	var e float64 = 0
+
+    for i:=0; i<n; i++{
+		var j float64 = float64(i)
+		
+        var factorial float64 = 1 
+        for j > 0 {
+            factorial = factorial * j
+			j = j- 1
+		}
+        e = e + 1/factorial
+	
+	}
+	fmt.Println(e)
+
 }
